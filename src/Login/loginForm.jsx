11b6f5b0bc77b1c8089d8446961a73475041e267
Cybre3 <cybre3@gmail.com>
2022-12-11 16:17:10 -0600

    Update Login page form
    changed simple form to dual grid, side 1 form, side 2 background image with heading and message.
    added picture of inside school to assets

--- src/Login/loginForm.jsx
+++ src/Login/loginForm.jsx
@@ -5,14 +5,25 @@ import '../common/mainBackground.css';
 class LoginForm extends Component {
   render() {
     return (
-      <div className="loginForm">
-        <h1>Login</h1>
+      <div className="login-container">
+        <div className="welcome-back">
+          <div className="welcome-back-box">
+            <div>Welcome</div>
+            <div>Back</div>
+            <p className="message">
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+              praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+              obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil.
+            </p>
+          </div>
+        </div>
         <form action="">
+          <h1>Sign in</h1>
           <label htmlFor="">Username</label>
           <input type="text" label="password" />
           <label htmlFor="">Password</label>
           <input type="password" label="password" />
-          <button>Login</button>
+          <button>Sign-In</button>
         </form>
       </div>
     );
