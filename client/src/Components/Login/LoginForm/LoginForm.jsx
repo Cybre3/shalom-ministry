8d0e97a7cbfe67eadf2970fbd7bf4845ed7efd3e
Cybre3 <cybre3@gmail.com>
2023-03-23 15:08:55 -0500

    client: updated build package

--- client/src/Components/Login/LoginForm/LoginForm.jsx
+++ client/src/Components/Login/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, Navigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import Joi from 'joi-browser';
 import Form from '../../common/form';
