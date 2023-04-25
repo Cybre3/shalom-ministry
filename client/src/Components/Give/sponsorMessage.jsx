ee03718b23aa919231c9ce9afba8392863a3413d
Cybre3 <cybre3@gmail.com>
2023-04-24 22:29:55 -0400

    client: updated css for forms

---
+++ client/src/Components/Give/sponsorMessage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SponsorMessage = () => {
+  return (
+    <div className="sponsor-message">
+      Thank you so much for your gift! <br /> <br /> Our written expression of thanks cannot compare
+      to that which we feel in our hearts. Shalom Ministry is more than an organization; it is an
+      organism made up of members of the Body of Christ. <br /> <br /> As sponsors, partners,
+      participants, and givers, you too are a member of The Body. As one, we operate as the
+      manifestation of Christ’s hands and feet on the earth, doing to Him what is done to the least
+      of these (Matt. 25:40). <br /> <br /> So again, we offer our deepest heartfelt thanks for your
+      gift - for you are giving to the Kingdom of God. Shalom! <br /> <br /> 1 Cor. 12:12-18
+    </div>
+  );
+};
+
+export default SponsorMessage;
