4cd319720bf727020146cebdd4e28f8e35e35640
Cybre3 <Cybre3@gmail.com>
2024-09-01 18:24:15 -0500

    client: adjust scripture mobile view

--- client/src/Components/common/Scripture.jsx
+++ client/src/Components/common/Scripture.jsx
@@ -1,30 +1,30 @@
 const Scripture = () => {
+  const scriptureText = (
+    <>
+      The Spirit of the Sovereign Lord is on me, <br /> because the Lord has anointed me <br /> to
+      proclaim good news to the poor.
+    </>
+  );
 
-    const scriptureText = <>The Spirit of the Sovereign Lord is on me, <br /> because the Lord has anointed me <br /> to proclaim good news to the poor.</>;
+  const scriptureSubText = `For individuals, families, communities, and organizations, discover how Shalom Ministry can impact you.`;
 
-    const scriptureSubText = `For individuals, families, communities, and organizations, discover how Shalom Ministry can impact you.`;
+  const scriptureReference = [<span>Isaiah 61:1</span>, <span>Luke 4:18</span>];
 
-    const scriptureReference = [<span>Isaiah 61:1</span>, <span>Luke 4:18</span>]
-
-    return (
-        <div className='w-screen h-screen bg-scriptureBlue bg-cover bg-center bg-no-repeat mt-10'>
-            <div className='container flex flex-col w-full h-full mx-auto items-center justify-center'>
-
-                <div className='flex flex-col h-1/2 text-white italic mb-[-40px]'>
-                    <p className='text-[5rem] font-thin w-full leading-[7rem]'>
-                        {scriptureText}
-                        <span className='ml-3 text-2xl space-x-3'>
-                            {scriptureReference.map(text => text)}
-                        </span>
-                    </p>
-                    <p className='text-blue-800 text-3xl italic'>
-                        {scriptureSubText}
-                    </p>
-                </div>
-
-            </div>
+  return (
+    <div className="lg:mt-10 h-screen w-screen bg-scriptureBlue bg-cover bg-center bg-no-repeat">
+      <div className="container mx-auto flex h-full w-full flex-col items-center justify-center">
+        <div className="mb-[-40px] flex h-1/2 flex-col italic text-white">
+          <p className="w-full px-3 text-3xl font-thin lg:text-[5rem] lg:leading-[7rem]">
+            {scriptureText}
+            <span className="ml-3 space-x-3 text-xl lg:text-2xl">
+              {scriptureReference.map((text) => text)}
+            </span>
+          </p>
+          <p className="px-3 text-justify italic text-blue-800 lg:text-3xl">{scriptureSubText}</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Scripture;
--- client/src/Components/common/missionStatement/MissionStatement.jsx
+++ client/src/Components/common/missionStatement/MissionStatement.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './size.css';
 
 function MissionStatement(props) {
+  const { innerWidth: width } = window;
+
   const uppercaseStatement = (
     <p>
       {' '}
@@ -18,6 +20,8 @@ function MissionStatement(props) {
     </p>
   );
 
+  const missionStatementText = `Shalom Ministry meets the needs of the lost, broken, and wounded as Yeshua’s extension of holistic help to those in need. We are a non-profit organization whose ambassadors compassionately listen to the individuals we serve to understand their unique needs, leading to their transformation. Our ministry began in 2018, when our founder was sent by Yeshua to Eritrea and Ghana, where she preached the Gospel, helped build schools, and provided facilities to the underprivileged population of the region. These projects serve as Shalom Ministry's heritage, which are still supported! Today and tomorrow, we seek to be the answer, the missing link, and the hands and feet of Yeshua on Earth!`;
+
   const statement = (
     <span>
       "Shalom Ministry <br /> meets the <br /> needs of the lost, broken,
@@ -25,22 +29,22 @@ function MissionStatement(props) {
     </span>
   );
 
-  const statementText = 'Shalom Ministry meets the needs of the lost, broken, & wounded';
+  const statementText = 'Shalom Ministry meets the needs of the lost, broken, & wounded...';
 
   return (
     <div className="h-screen w-screen bg-shalom-watermark bg-[length:50%] bg-[-30%_60%] bg-no-repeat">
       <div className="relative h-fit w-full text-center backdrop-blur lg:pb-32">
         <div
           before-dynamic-value={statementText}
-          className={`lg:before:content-[" "] container mx-auto items-center justify-center before:absolute before:right-0 before:h-full before:w-full before:rounded-md before:flex before:items-center before:leading-[6rem] before:border-8 before:border-x-0 before:border-b-0 before:border-violet-900 before:bg-violet-200 before:text-6xl before:content-[attr(before-dynamic-value)] before:text-right before:pr-10 lg:w-auto lg:before:right-24 lg:before:w-5/6 before:opacity-20`}
+          className={`lg:before:content-[""] lg:before:[] container mx-auto items-center justify-center before:absolute before:right-0 before:flex before:h-full before:w-full before:items-center before:rounded-md before:border-8 before:border-x-0 before:border-b-0 before:border-violet-900 before:bg-violet-200 before:pl-4 before:pr-3 before:text-right before:text-6xl before:leading-[6rem] before:opacity-10 before:content-[attr(before-dynamic-value)] lg:w-auto lg:before:right-24 lg:before:w-5/6 lg:before:opacity-20`}
         >
           <h2 className="pt-10 text-4xl lg:text-8xl">
             MISSION <span className="">&</span> VISION
           </h2>
 
-          <div className="mx-0 my-4 lg:my-0 h-full w-full items-start justify-center px-10 lg:flex lg:h-3/4 lg:p-12">
-            <p className="text-justify lg:text-left text-lg lg:text-2xl lg:leading-[3.2rem]">
-              {uppercaseStatement}
+          <div className="mx-0 my-4 h-full w-full items-start justify-center px-9 pb-10 lg:my-0 lg:flex lg:h-3/4 lg:p-12">
+            <p className="text-justify text-lg lg:text-left lg:text-2xl lg:leading-[3.2rem]">
+              {width > 1024 ? uppercaseStatement : missionStatementText}
             </p>
             <p className="ml-[-155px] hidden w-1/4 text-right text-2xl lg:mt-[-10px] lg:block lg:text-6xl lg:leading-[5.5rem]">
               {statement}
