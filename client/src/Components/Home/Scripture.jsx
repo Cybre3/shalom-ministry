d15594376a7b13a44de55e8e4b0171e7fe228f4a
Cybre3 <Cybre3@gmail.com>
2025-02-04 17:12:48 -0600

    client: scripture and umbrella responsive fix

--- client/src/Components/Home/Scripture.jsx
+++ client/src/Components/Home/Scripture.jsx
@@ -14,7 +14,7 @@ const Scripture = () => {
     <div className="h-screen w-screen lg:mt-10">
       <div className="mx-auto flex h-full w-full items-center justify-center">
         <div className="m-12 flex h-3/4 w-full flex-col items-center justify-center rounded-lg border-4 border-blue-700/30 bg-scriptureBlue bg-cover bg-center bg-no-repeat text-center italic text-white shadow-xl shadow-blue-500/60 lg:text-left">
-          <p className="mb-10 w-full px-3 text-center text-3xl font-thin lg:text-[5rem] lg:leading-[7rem]">
+          <p className="mt-24 lg:mt-0 mb-10 w-full px-3 text-center text-3xl font-thin lg:text-[5rem] lg:leading-[7rem]">
             {scriptureText}
             <span className="ml-3 space-x-3 text-xl lg:text-2xl">
               {scriptureReference.map((text) => text)}
--- client/src/Components/Home/Umbrella.jsx
+++ client/src/Components/Home/Umbrella.jsx
@@ -28,7 +28,7 @@ function Umbrella() {
   const { innerWidth: width } = window;
 
   return (
-    <div className="h-fit w-screen py-20 text-center bg-violet-300/20">
+    <div className="h-fit w-screen bg-violet-300/20 py-20 text-center">
       <div className="h-fit w-full">
         <h2 className="mb-4 w-full px-2 text-4xl lg:text-8xl">
           {width > 1024 ? 'The SHALOM MINISTRY Umbrella' : 'SHALOM MINISTRY Umbrella'}
@@ -41,25 +41,33 @@ function Umbrella() {
           Ministry oversees!
         </p>
 
-        <div className="mx-10 my-20 flex w-2/3 justify-around justify-self-center lg:mx-32">
-          {width > 1024
-            ? items.map((item) => (
-                <div className="h-150px flex w-1/3 flex-col items-center justify-center rounded-3xl border-2 border-sky-900 bg-black py-8 pt-10 hover:shadow-sky-900 hover:shadow-lg">
-                  <img
-                    className="mb-auto h-[350px] w-[400px] rounded-lg bg-white p-2"
-                    src={item.src}
-                    alt={item.alt}
-                  />
-                  <p className="mb-auto w-full px-5 pt-6 text-2xl">{item.text}</p>
-                </div>
-              ))
-            : items.map((item) => (
-                <div className="mb-12 flex flex-col items-center justify-center">
-                  <img className="h-[350px] w-[400px]" src={item.src} alt={item.alt} />
-                  <p className="mb-2 w-auto pt-3 text-2xl">{item.text}</p>
-                </div>
-              ))}
-        </div>
+        {width > 1024 ? (
+          <div className="my-20 flex w-2/3 justify-around justify-self-center mx-32">
+            {items.map((item) => (
+              <div className="h-150px flex w-1/3 flex-col items-center justify-center rounded-3xl border-2 border-sky-900 bg-black py-8 pt-10 hover:shadow-lg hover:shadow-sky-900">
+                <img
+                  className="mb-auto h-[350px] w-[400px] rounded-lg bg-white p-2"
+                  src={item.src}
+                  alt={item.alt}
+                />
+                <p className="mb-auto w-full px-5 pt-6 text-2xl">{item.text}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="mx-10 my-20 flex-col w-2/3 justify-around justify-self-center space-y-10">
+            {items.map((item) => (
+              <div className="h-150px flex w-full flex-col items-center justify-center rounded-3xl border-2 border-sky-900 bg-black py-8 pt-10 hover:shadow-lg hover:shadow-sky-900">
+                <img
+                  className="mb-auto h-[250px] w-[400px] rounded-lg bg-white p-2"
+                  src={item.src}
+                  alt={item.alt}
+                />
+                <p className="mb-auto w-full px-5 pt-6 text-2xl">{item.text}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
